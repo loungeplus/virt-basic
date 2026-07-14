2895bd9a4e8871be26f8b2f1ecdd6ef5a38c84ab
capsmalt <capsmalt@gmail.com>
2026-07-14 17:29:25 +0900

    Change GIT_REPO_URL in translation.sh script
    Updated the GIT_REPO_URL in the deployment patch for showroom.

--- translation.sh
+++ translation.sh
@@ -5,6 +5,6 @@ for ((i=1; i<=line_count; i++)); do
     NS="showroom-user"$i
     oc scale -n $NS deploy/showroom --replicas=0
     sleep 1
-    oc patch -n $NS deploy/showroom --patch='{"spec":{"template":{"spec":{"containers": [{"name": "content","env": [{"name": "GIT_REPO_URL", "value": "https://github.com/mayumi00/ocp-virt-roadshow-2026-showroom"}, {"name": "GIT_REPO_REF", "value": "main"}]}]}}}}'
+    oc patch -n $NS deploy/showroom --patch='{"spec":{"template":{"spec":{"containers": [{"name": "content","env": [{"name": "GIT_REPO_URL", "value": "https://github.com/loungeplus/virt-basic"}, {"name": "GIT_REPO_REF", "value": "main"}]}]}}}}'
     oc scale -n $NS deploy/showroom --replicas=1
 done
